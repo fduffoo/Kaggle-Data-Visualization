3df8a6448999c60924442ab6270db4d44d90d448
FernandoDuffoo <fduffoo@aol.com>
2024-02-23 20:26:59 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# miniproject2FernandoDuffoo
+### INF601 - Advanced Programming in Python
+### Fernando Duffoo
+### Mini Project 2
+
+
+# Project Title
+
+Mini Project 2
+
+## Description
+
+This project will be using Pandas and Faker to create dataframes.
+
+## Getting Started
+
+### Dependencies
+
+* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+* ex. Windows 10
+* List pip install instructions.
+
+### Installing
+
+* How/where to download your program
+* Any modifications needed to be made to files/folders
+
+### Executing program
+
+* How to run the program
+* Step-by-step bullets
+```
+code blocks for commands
+```
+
+## Help
+
+Any advise for common problems or issues.
+```
+command to run if program contains helper info
+```
+
+## Authors
+
+Contributors names and contact info
+
+ex. Dominique Pizzie  
+ex. [@DomPizzie](https://twitter.com/dompizzie)
+
+## Version History
+
+* 0.2
+    * Various bug fixes and optimizations
+    * See [commit change]() or See [release history]()
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [awesome-readme](https://github.com/matiassingers/awesome-readme)
+* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
+* [dbader](https://github.com/dbader/readme-template)
+* [zenorocha](https://gist.github.com/zenorocha/4526327)
+* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
---
+++ main.py
@@ -0,0 +1,149 @@
+#  INF601 - Advanced Programming in Python
+#  Fernando Duffoo
+#  Mini Project 2
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+# Load the data into a Pandas DataFrame
+drugs = pd.read_csv("drug-overdose-death-rates new.csv", parse_dates=True)
+
+# Create 'charts' folder if it doesn't exist
+os.makedirs('charts', exist_ok=True)
+
+# Plotting the overall drug overdose death rates as a bar chart
+plt.figure(figsize=(12, 6))
+for column in drugs.columns:
+    plt.bar(drugs.index, drugs[column], label=column, alpha=0.7)
+
+# Adding labels and legend
+plt.xlabel('Date')
+plt.ylabel('Death Rates')
+plt.title('Overall Drug Overdose Death Rates in the US')
+plt.legend()
+
+# Formatting date on x-axis
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+
+# Add a horizontal line to separate the two graphs
+plt.axvline(x=drugs.index[-1], color='red', linestyle='--', label='Separation Line')
+
+# Save the overall plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/overall_drug_overdose_death_rates.png')
+
+# Show the overall plot
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Plotting deaths from cocaine overdose only as a scatter plot
+plt.figure(figsize=(12, 6))
+plt.scatter(drugs.index, drugs['Cocaine overdose death rates (CDC WONDER)'], label='Cocaine', s=50, alpha=0.7)
+
+# Adding labels and legend
+plt.xlabel('Date')
+plt.ylabel('Death Rates')
+plt.title('Cocaine Overdose Death Rates in the US')
+plt.legend()
+
+# Formatting date on x-axis
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+
+# Add a horizontal line to mark the separation
+plt.axvline(x=drugs.index[-1], color='red', linestyle='--', label='Separation Line')
+
+# Save the cocaine plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/cocaine_overdose_death_rates.png')
+
+# Show the cocaine plot
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Plotting deaths from opioid overdose only as a box plot
+plt.figure(figsize=(12, 6))
+plt.boxplot(drugs['Any opioid death rates (CDC WONDER)'])
+
+# Adding labels and legend
+plt.xlabel('Opioid Overdose Death Rates')
+plt.title('Opioid Overdose Death Rates in the US')
+
+# Save the opioid plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/opioid_overdose_death_rates.png')
+
+# Show the opioid plot
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Plotting deaths from heroin overdose only as a line plot
+plt.figure(figsize=(12, 6))
+plt.plot(drugs.index, drugs['Heroin overdose death rates (CDC WONDER)'], label='Heroin', linestyle='-', marker='o', markersize=5, alpha=0.7)
+
+# Adding labels and legend
+plt.xlabel('Date')
+plt.ylabel('Death Rates')
+plt.title('Heroin Overdose Death Rates in the US')
+plt.legend()
+
+# Formatting date on x-axis
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+
+# Add a horizontal line to mark the separation
+plt.axvline(x=drugs.index[-1], color='red', linestyle='--', label='Separation Line')
+
+# Save the heroin plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/heroin_overdose_death_rates.png')
+
+# Show the heroin plot
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Plotting deaths from synthetic drug overdoses as a step plot
+plt.figure(figsize=(12, 6))
+plt.step(drugs.index, drugs['Synthetic opioids death rates (CDC WONDER)'], label='Synthetic', where='post', linestyle='-', marker='o', markersize=5, alpha=0.7)
+
+# Adding labels and legend
+plt.xlabel('Date')
+plt.ylabel('Death Rates')
+plt.title('Synthetic Drug Overdose Death Rates in the US')
+plt.legend()
+
+# Formatting date on x-axis
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+
+# Add a horizontal line to mark the separation
+plt.axvline(x=drugs.index[-1], color='red', linestyle='--', label='Separation Line')
+
+# Save the synthetic plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/synthetic_overdose_death_rates.png')
+
+# Show the synthetic plot
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Plotting deaths from prescription drug overdoses as a filled area plot
+plt.figure(figsize=(12, 6))
+plt.fill_between(drugs.index, 0, drugs['Prescription Opioids death rates (US CDC WONDER)'], label='Prescription', alpha=0.7)
+
+# Adding labels and legend
+plt.xlabel('Date')
+plt.ylabel('Death Rates')
+plt.title('Prescription Drug Overdose Death Rates in the US')
+plt.legend()
+
+# Formatting date on x-axis
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+
+# Add a horizontal line to mark the separation
+plt.axvline(x=drugs.index[-1], color='red', linestyle='--', label='Separation Line')
+
+# Save the prescription plot as a PNG file inside the 'charts' folder
+plt.savefig('charts/prescription_overdose_death_rates.png')
+
+# Show the prescription plot
+plt.show()
